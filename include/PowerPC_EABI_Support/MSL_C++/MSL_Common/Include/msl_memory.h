3431f9defd7c0c75873cccdb3e28b2e9ebafa4b7
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2025-11-25 23:06:50 +0000

    Update msl_memory.h
    this fixes build errors for most of the current files

--- include/PowerPC_EABI_Support/MSL_C++/MSL_Common/Include/msl_memory.h
+++ include/PowerPC_EABI_Support/MSL_C++/MSL_Common/Include/msl_memory.h
@@ -3,30 +3,47 @@
 
 namespace std {
 
-template <class ForwardIt, class Size, class T>
-inline ForwardIt uninitialized_fill_n(ForwardIt first, Size count, const T& value)
-{
-	for (; count > 0; ++first, (void)--count) {
-		*first = value;
-	}
-	return first;
+template<class ForwardIt, class Size, class T>
+inline ForwardIt uninitialized_fill_n(ForwardIt first, Size count, const T& value) {
+    for (; count--; ++first) {
+        if (first != NULL) {
+            *first = value;
+        }
+    }
+    return first;
 }
 
-template <class T, int A, int B>
-class __uninitialized_copy_helper {
-	static T* uninitialized_copy(T* begin, T* end, T* dest)
-	{
-		for (; begin < end; ++begin, ++dest) {
-			*dest = *begin;
-		}
-		return dest;
+template<class InputIterator, class ForwardIterator>
+inline ForwardIterator __uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result) {
+    ForwardIterator __save = result;
+
+    for (; first != last; ++first, ++result) {
+        *result = *first;
+    }
+    return result;
+}
+
+template <class T, bool A, bool B>
+struct __uninitialized_copy_helper {
+	static T* uninitialized_copy(T* first, T* last, T* result) {
+		return __uninitialized_copy(first, last, result);
 	}
 };
 
 template <class T>
-inline T* uninitialized_copy(T* first, T* last, T* d_first)
+struct __uninitialized_copy_helper<T, true, false>
 {
-	return __uninitialized_copy_helper<T, 0, 1>::uninitialized_copy(first, last, d_first);
+	static T* uninitialized_copy(T* first, T* last, T* result)
+	{
+		for (; first < last; ++result, ++first)
+			*result = *first;
+		return result;
+	}
+};
+
+template <class T>
+inline T* uninitialized_copy(T* first, T* last, T* result) {
+	return __uninitialized_copy_helper<T, true, false>::uninitialized_copy(first, last, result);
 }
 
 } // namespace std
