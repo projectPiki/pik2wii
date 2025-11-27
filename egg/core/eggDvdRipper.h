cff0bf9eba7db6bdea958d320383c38b5acb91c3
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2025-11-27 13:45:18 +0000

    start egg
    eggDvdRipper and eggMatrix from skyward sword, eggVector and eggDecomp from ogws

---
+++ egg/core/eggDvdRipper.h
@@ -0,0 +1,27 @@
+#ifndef EGG_DVD_RIPPER_H
+#define EGG_DVD_RIPPER_H
+
+#include "egg/egg_types.h"
+
+namespace EGG {
+
+class DvdRipper {
+public:
+    enum EAllocDirection { ALLOC_DIR_TAIL, ALLOC_DIR_HEAD };
+
+public:
+    static u8* loadToMainRAM(const char* pPath, u8* pBuffer, Heap* pHeap,
+                             EAllocDirection allocDir, u32 offset, u32* pRead,
+                             u32* pSize);
+
+    static u8* loadToMainRAM(DvdFile* pFile, u8* pBuffer, Heap* pHeap,
+                             EAllocDirection allocDir, u32 offset, u32* pRead,
+                             u32* pSize);
+
+private:
+    static bool sErrorRetry;
+};
+
+} // namespace EGG
+
+#endif
